abcbe335a58a8f86b8445aa05d62c45a982f718a
indhuvsb <36696610+indhuvsb@users.noreply.github.com>
2018-03-16 16:21:59 +0530

    mimii.C
    good

---
+++ avg.C
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main( ) 
+{ 
+      int  n, count = 1; 
+      float  x, average, sum = 0; 
+      printf("How many Numbers? " ) ; 
+      scanf ("%d",&n) ; 
+      while (count < n) 
+             { 
+                  printf ("x = " ) ; 
+                  scanf("%f", &x); 
+               }
+                  average = sum/n; 
+                  printf("\nThe Average is %f\n", average); 
+} 
